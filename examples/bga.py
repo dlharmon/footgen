bc8d555e09078f270e59762d80aaec43ab8a8b67
Darrell Harmon <darrell@harmoninstruments.com>
2014-12-11 11:42:17 -0700

    add DDR3 FBGA96, Xilinx CLG225, CLG400

--- examples/bga.py
+++ examples/bga.py
@@ -17,3 +17,21 @@ f.diameter = 0.45
 f.bga(26, omit = "A1,B1,G1,H1,N1,P1,W1,Y1,AE1,AF1,J9")
 f.silkbox(w=27, notch=0.75)
 f.finish()
+
+bga_names = ["CLG225", "CLG400"]
+bga_columns = [15, 20]
+for i in range(len(bga_names)):
+    f = footgen.Footgen(bga_names[i])
+    f.pitch = 0.8
+    f.diameter = 0.4
+    f.bga(bga_columns[i])
+    f.silkbox(w=f.pitch*bga_columns[i]+f.pitch, notch=0.75)
+    f.finish()
+
+# DDR3 RAM
+f = footgen.Footgen("FBGA96")
+f.pitch = 0.8
+f.diameter = 0.4
+f.bga(rows = 16, columns = 9, omit = "A4:T6")
+f.silkbox(w=9, h=13, notch=0.5)
+f.finish()
