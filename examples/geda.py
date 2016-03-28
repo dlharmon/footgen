e9efc465c589fcb536f8b78df038288e050b23a9
Darrell Harmon <darrell@harmoninstruments.com>
2016-03-27 21:22:16 -0600

    add geda test

---
+++ examples/geda.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from footgen import *
+
+f = Footgen("gedatest", output_format="geda")
+f.qfn(pitch=0.5, pins=16, pinswide = 3, width=2.2, height = 2.5, padheight=0.254, padwidth=0.66, silk_xsize=3.0)
+f.thermal_pad(1.66, pin=17)
+f.via_array(columns=2, pitch=1.0, size=0.2, pad=0.5, pin = 17)
+f.finish()
