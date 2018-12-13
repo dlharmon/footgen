0c58c3bdd08b31336bd32d93506837870627daa0
Darrell Harmon <darrell@harmoninstruments.com>
2018-12-13 14:18:15 -0700

    pcie example: move board outline to Edge.Cuts from silk

--- examples/pcie.py
+++ examples/pcie.py
@@ -26,28 +26,26 @@ def pciecard(npins, name):
             x += 3.0 # mm, key
         else:
             x += 1.0 # mm
-    g.silk_line(0.0, -4.5, 15.0, -4.5)
-    g.silk_line(15.0, -4.5, 15.0, -12.75)
-    g.silk_line(15.0, -12.75, 33.35, -12.75)
-    g.silk_line(33.35, -12.75, 33.35, -4.5)
-    g.silk_line(33.35, -4.5, 41.35, -4.5) # pci block lower edge
-    g.silk_line(41.35, -4.5, 41.35, -10.925)
-    g.silk_arc(43.175, -10.925, 41.35, -10.925, 180)
-    g.silk_line(45.0, -10.925, 45.0, -0.7)
-    g.silk_line(45.7, 0, 45.0, -0.7) # diagonal at 1
-    g.silk_line(45.7, 0, 56.2 - 0.7, 0) # lower edge contacts 1 - 11
-    g.silk_line(56.2 - 0.7, 0, 56.2, -0.7) # diagonal at 11
-    g.silk_line(56.2, -0.7, 56.2, -8.0)
-    g.silk_arc(57.15, -8.0, 56.2, -8.0, 180)
-    g.silk_line(58.1, -8.0, 58.1, -0.7)
-    g.silk_line(58.1 + 0.7, 0, 58.1, -0.7) # diagonal at 12
+    g.silk_line(0.0, -4.5, 15.0, -4.5, layer='Edge.Cuts', width=0.1)
+    g.silk_line(15.0, -4.5, 15.0, -12.75, layer='Edge.Cuts', width=0.1)
+    g.silk_line(15.0, -12.75, 33.35, -12.75, layer='Edge.Cuts', width=0.1)
+    g.silk_line(33.35, -12.75, 33.35, -4.5, layer='Edge.Cuts', width=0.1)
+    g.silk_line(33.35, -4.5, 41.35, -4.5, layer='Edge.Cuts', width=0.1) # pci block lower edge
+    g.silk_line(41.35, -4.5, 41.35, -10.925, layer='Edge.Cuts', width=0.1)
+    g.silk_arc(43.175, -10.925, 41.35, -10.925, 180, layer='Edge.Cuts', width=0.1)
+    g.silk_line(45.0, -10.925, 45.0, -0.7, layer='Edge.Cuts', width=0.1)
+    g.silk_line(45.7, 0, 45.0, -0.7, layer='Edge.Cuts', width=0.1) # diagonal at 1
+    g.silk_line(45.7, 0, 56.2 - 0.7, 0, layer='Edge.Cuts', width=0.1) # lower edge contacts 1 - 11
+    g.silk_line(56.2 - 0.7, 0, 56.2, -0.7, layer='Edge.Cuts', width=0.1) # diagonal at 11
+    g.silk_line(56.2, -0.7, 56.2, -8.0, layer='Edge.Cuts', width=0.1)
+    g.silk_arc(57.15, -8.0, 56.2, -8.0, 180, layer='Edge.Cuts', width=0.1)
+    g.silk_line(58.1, -8.0, 58.1, -0.7, layer='Edge.Cuts', width=0.1)
+    g.silk_line(58.1 + 0.7, 0, 58.1, -0.7, layer='Edge.Cuts', width=0.1) # diagonal at 12
     cardedge_endx = 65.3 + 0.5*(npins-36)
-    g.silk_line(58.1 + 0.7, 0.0, cardedge_endx - 0.7, 0.0) # lower edge 2nd contacts
-    g.silk_line(cardedge_endx -0.7, 0.0, cardedge_endx, -0.7)
-    g.silk_line(cardedge_endx, -0.7, cardedge_endx, -13.0)
-    output_file = open(name + ".kicad_mod", "w")
-    output_file.write(g.finish())
-    output_file.close()
+    g.silk_line(58.1 + 0.7, 0.0, cardedge_endx - 0.7, 0.0, layer='Edge.Cuts', width=0.1) # lower edge 2nd contacts
+    g.silk_line(cardedge_endx -0.7, 0.0, cardedge_endx, -0.7, layer='Edge.Cuts', width=0.1)
+    g.silk_line(cardedge_endx, -0.7, cardedge_endx, -13.0, layer='Edge.Cuts', width=0.1)
+    f.finish()
 
 pciecard(36, "pciex1fingers")
 pciecard(64, "pciex4fingers")
